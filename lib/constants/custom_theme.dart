4d5e6026202def7f466aa8ed25751fecd11cffd4
sayyad-asifbasha <asifbasha4873@gmail.com>
2025-05-25 21:22:19 +0530

    made changes

--- lib/constants/custom_theme.dart
+++ lib/constants/custom_theme.dart
@@ -16,8 +16,14 @@ class RockMusicTheme {
   static const Color _lightOnPrimaryFixed = Color(0xFF550120);
   static const Color _lightPrimaryFixedDim = Color(0xFF61616B);
   static const Color _lightonPrimaryFixedVariant = Color(0xFF2C2D31);
-
-
+  static const Color _lightSecondaryColor = Color(0xFF2F2F39);
+  static const Color _lightOnSecondaryColor = Color(0xFF2F2F39);
+  static const Color _lightSecondaryContainer = Color(0xFF61616B);
+  static const Color _lightonSecondaryContainer = Color(0xFF000000);
+  static const Color _lightSecondaryFixed = Color(0xFF18171C);
+  static const Color _lightOnSecondaryFixed = Color(0xFF550120);
+  static const Color _lightSecondaryFixedDim = Color(0xFF61616B);
+  static const Color _lightonSecondaryFixedVariant = Color(0xFF2C2D31);
 
   /// Theme for light mode.
   static final lightTheme = ThemeData(
@@ -28,6 +34,7 @@ class RockMusicTheme {
     primaryColor: _lightPrimaryColor,
 
     textTheme: _lightTextTheme,
+    inputDecorationTheme: _lightInputDecorationTheme,
     colorScheme: const ColorScheme.light(
       primary: _lightPrimaryColor,
       onPrimary: _lightOnPrimaryColor,
@@ -37,22 +44,22 @@ class RockMusicTheme {
       onPrimaryFixed: _lightOnPrimaryFixed,
       primaryFixedDim: _lightPrimaryFixedDim,
       onPrimaryFixedVariant: _lightonPrimaryFixedVariant,
+      secondary: _lightSecondaryColor,
+      onSecondary: _lightOnSecondaryColor,
+      secondaryContainer: _lightSecondaryContainer,
+      onSecondaryContainer: _lightonSecondaryContainer,
+      secondaryFixed: _lightSecondaryFixed,
+      onSecondaryFixed: _lightOnSecondaryFixed,
+      secondaryFixedDim: _lightSecondaryFixedDim,
+      onSecondaryFixedVariant: _lightonSecondaryFixedVariant,
+
     ).copyWith(secondary: _lightAccentColor),
   );
 
   static const TextTheme _lightTextTheme = TextTheme(
-    headlineMedium: TextStyle(
-      fontWeight: FontWeight.w700,
-      fontSize: 24,
-    ),
-    headlineSmall: TextStyle(
-      color: Colors.black,
-      fontSize: 20,
-    ),
-    titleLarge: TextStyle(
-      fontSize: 16,
-      color: Color(0xFF737373),
-    ),
+    headlineMedium: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+    headlineSmall: TextStyle(color: Colors.black, fontSize: 20),
+    titleLarge: TextStyle(fontSize: 16, color: Color(0xFF737373)),
     bodyLarge: TextStyle(fontSize: 14, color: Color(0xFF737373)),
     bodyMedium: TextStyle(fontSize: 14),
     bodySmall: TextStyle(
@@ -61,4 +68,30 @@ class RockMusicTheme {
       fontSize: 12.0,
     ),
   );
+  static final InputDecorationTheme _lightInputDecorationTheme =
+      InputDecorationTheme(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12,
+        ),
+        prefixIconColor: _lightOnPrimaryColor,
+        hintStyle: _lightTextTheme.titleLarge?.copyWith(
+          color: _lightSecondaryContainer,
+          fontWeight: FontWeight.bold,
+        ),
+        filled: true,
+        fillColor: _lightOnSecondaryColor,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      );
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -16,70 +16,214 @@ class _HomeScreenState extends State<HomeScreen> {
     return BaseView<HomeScreenViewModel>(
       builder: (context, model, child) {
         return Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 0,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            automaticallyImplyLeading: false,
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(SizeConfig.screenHeight! * 0.38),
+              child: Container(
+                width: double.infinity,
+                height: SizeConfig.screenHeight! * 0.38,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 12,
+                ),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.primary,
+                      Theme.of(context).colorScheme.primary,
+                      Theme.of(context).colorScheme.onPrimaryFixed,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: const [0.0, 0.5, 1.0],
+                  ),
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20),
+                    Container(
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.search_rounded),
+                                prefixIconColor:
+                                    Theme.of(context).colorScheme.onPrimary,
+                                hintText: "Search \"Punjab Lyrics\" ",
+                                hintStyle: Theme.of(
+                                  context,
+                                ).textTheme.titleMedium?.copyWith(
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).colorScheme.secondaryContainer,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                suffixIcon: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      "|",
+                                      style: TextStyle(
+                                        color:
+                                            Theme.of(
+                                              context,
+                                            ).colorScheme.secondaryContainer,
+                                        fontSize:
+                                            SizeConfig.blockSizeVertical! * 3.5,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    Icon(
+                                      Icons.mic,
+                                      size: SizeConfig.blockSizeVertical! * 2.8,
+                                      color:
+                                          Theme.of(
+                                            context,
+                                          ).colorScheme.onPrimary,
+                                    ),
+                                  ],
+                                ),
+                              ),
+
+                              // decoration: InputDecoration(
+                              //   contentPadding: EdgeInsets.symmetric(
+                              //     horizontal: 16,
+                              //     vertical: 14,
+                              //   ),
+                              //   prefixIcon: Icon(Icons.search_rounded),
+                              //   prefixIconColor:
+                              //       Theme.of(context).colorScheme.onPrimary,
+                              //   suffixIcon: Container(
+                              //     child: Row(
+                              //       mainAxisAlignment: MainAxisAlignment.start,
+                              //       mainAxisSize: MainAxisSize.min,
+                              //       children: [
+                              //         Text(
+                              //           "|",
+                              //           style: TextStyle(
+                              //             fontSize:
+                              //                 SizeConfig.blockSizeVertical! * 3.5,
+                              //             color :Theme.of(context).colorScheme.secondaryContainer,
+                              //
+                              //     ),
+                              //         ),
+                              //         SizedBox(width: 8),
+                              //         Icon(
+                              //           Icons.mic,
+                              //           size: SizeConfig.blockSizeVertical! * 2.8,
+                              //           color: Theme.of(context).colorScheme.onPrimary,
+                              //         ),
+                              //       ],
+                              //     ),
+                              //   ),
+                              //   hintText: "Search \"Punjab Lyrics\" ",
+                              //   hintStyle: Theme.of(
+                              //     context,
+                              //   ).textTheme.titleMedium?.copyWith(
+                              //     color:
+                              //         Theme.of(context).colorScheme.secondaryContainer,
+                              //     fontWeight: FontWeight.bold,
+                              //   ),
+                              //   filled: true,
+                              //   fillColor: Theme.of(context).colorScheme.onSecondary,
+                              //   border: OutlineInputBorder(
+                              //     borderRadius: BorderRadius.circular(12),
+                              //     borderSide: BorderSide.none,
+                              //   ),
+                              //   enabledBorder: OutlineInputBorder(
+                              //     borderRadius: BorderRadius.circular(12),
+                              //     borderSide: BorderSide.none,
+                              //   ),
+                              //   focusedBorder: OutlineInputBorder(
+                              //     borderRadius: BorderRadius.circular(12),
+                              //     borderSide: BorderSide.none,
+                              //   ),
+                              // ),
+                              style: Theme.of(
+                                context,
+                              ).textTheme.titleMedium?.copyWith(
+                                color:
+                                    Theme.of(
+                                      context,
+                                    ).colorScheme.secondaryContainer,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              cursorColor:
+                                  Theme.of(
+                                    context,
+                                  ).colorScheme.secondaryContainer,
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(Icons.account_circle, color: Colors.white),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.blockSizeHorizontal! * 10),
+                    Text(
+                      "Claim your",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 4,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.blockSizeHorizontal! * 3),
+                    Text(
+                      "Free Demo",
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 7,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.blockSizeHorizontal! * 1),
+                    Text(
+                      "for custom Music Production",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 2,),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding:EdgeInsets.symmetric(vertical:0,horizontal: SizeConfig.safeBlockHorizontal!*3),
+                      ),
+                      child: Container(
+                        child: Text(
+                          "Book Now",
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium?.copyWith(
+                            fontSize: SizeConfig.blockSizeHorizontal! * 4.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
           body: SingleChildScrollView(
             child: SafeArea(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: SizeConfig.screenWidth,
-                    height: SizeConfig.screenHeight! * 0.35,
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Theme.of(context).colorScheme.primary,
-                          Theme.of(context).colorScheme.primary,
-                          Theme.of(context).colorScheme.primary,
-                          Theme.of(context).colorScheme.onPrimaryFixed,
-                        ],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        stops: [0.0, 0.4, 0.5, 0.99],
-                      ),
-                      borderRadius: const BorderRadius.only(
-                        bottomRight: Radius.circular(20),
-                        bottomLeft: Radius.circular(20),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 20),
-                        Row(
-                          children: [
-                            TextField(
-                              decoration: InputDecoration(
-                                labelText: 'Enter your name',
-                                labelStyle: TextStyle(
-                                  color: Theme.of(context).colorScheme.onPrimary,
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Theme.of(context).colorScheme.onPrimary,
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Theme.of(context).colorScheme.onPrimary,
-                                    width: 2,
-                                  ),
-                                ),
-                              ),
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.onPrimary,
-                              ),
-                              onChanged: (value) {
-                                print('User typed: $value');
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                children: [],
               ),
             ),
           ),
